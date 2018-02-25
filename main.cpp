d0aa69579e707a4fa53b2da9d7c928cfae0ad951
juany-ramirez <juany.ramrz@unitec.edu>
2018-02-25 13:04:02 -0600

    cambios en main html

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <vector>
 #include <unistd.h>
 
@@ -17,11 +18,24 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
+	FILE *html_data;
+	//r - read nada mas
+	html_data = fopen("index.html","r");
+	char response_data[1024];
+	//para leer el archivo y meterlo a response data
+	fgets(response_data, 1024,html_data);
+
+	//cambios para http protocol -status code
+	char http_header[2048] = "HTTP/1.1 200 OK\r\n\n";
+	strcat(http_header,response_data);
+
+
 
   	vector<pthread_t> ThreadPool;
   	int Client;
   	int Accept = 1;
   	char buffer[256]; //Buffer para recibir mensajes
+  	
   	//Server: Address domain del socket, Tipo de socket y Protocolo (0 indica que el sistema elige el mejor)
   	int Server = socket(AF_INET, SOCK_STREAM, 0);
   	if(Server < 0){
@@ -31,6 +45,7 @@ int main(int argc, char const *argv[]) {
   		cout << "Socket abierto con exito" <<endl;
 
   	}
+
   	struct sockaddr_in ServerAddress;
   	socklen_t SocketSize;
   	ServerAddress.sin_family = AF_INET;
@@ -47,8 +62,11 @@ int main(int argc, char const *argv[]) {
 	}
 
 	SocketSize = sizeof(ServerAddress);
-	cout<< "Esperando coneccion..."<<endl;
-	listen(Server, 1);
+	cout<< "Esperando conexion..."<<endl;
+	if(listen(Server, 1)==-1){
+		perror("Error on listen");
+		return 1;
+	}
 
 	while(true){
 		Client = accept(Server, (struct sockaddr*) &ServerAddress, &SocketSize);
@@ -59,6 +77,9 @@ int main(int argc, char const *argv[]) {
 		if(Client < 0 ){
 			cout << "Error conectando al server" << endl;
 		}
+		//mandar data al cliente con send
+		send(Client,http_header,sizeof(http_header),0);
+		close(Client);
 	}
   	close(Server);
 	return 0;
